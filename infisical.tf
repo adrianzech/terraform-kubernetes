2bbadb24a6b0e61b48fa0fe122703542f283fd26
Adrian Zech <adrian@zech.co>
2026-02-07 11:33:28 +0100

    fix: remove explicit TTL from infisical token reviewer

--- infisical.tf
+++ infisical.tf
@@ -29,6 +29,24 @@ resource "kubernetes_cluster_role_binding_v1" "infisical_token_reviewer" {
   }
 }
 
+resource "kubernetes_cluster_role_binding_v1" "infisical_service_account_token_reviewer" {
+  metadata {
+    name = "infisical-service-account-token-reviewer-role-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "system:auth-delegator"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "infisical-service-account"
+    namespace = kubernetes_namespace_v1.infisical.metadata[0].name
+  }
+}
+
 resource "kubernetes_token_request_v1" "infisical_token_reviewer" {
   metadata {
     name      = kubernetes_service_account_v1.infisical_token_reviewer.metadata[0].name
@@ -36,6 +54,5 @@ resource "kubernetes_token_request_v1" "infisical_token_reviewer" {
   }
 
   spec {
-    expiration_seconds = 3600
   }
 }
